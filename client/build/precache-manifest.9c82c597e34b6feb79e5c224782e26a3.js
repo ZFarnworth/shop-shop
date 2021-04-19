15f8fb03acaaa294e8b504d37bded81599761626
Zac <zacharyfarnworth@gmail.com>
2021-04-18 23:17:27 -0600

    fixing deploy

---
+++ client/build/precache-manifest.9c82c597e34b6feb79e5c224782e26a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c0183f04c47a9a511572f34c89a4ca4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff606587e7f22167108c",
+    "url": "/static/css/main.49b21ed8.chunk.css"
+  },
+  {
+    "revision": "16f4b1e124ecda48c1fa",
+    "url": "/static/js/2.8db5df9a.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.8db5df9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff606587e7f22167108c",
+    "url": "/static/js/main.6d7702d1.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
